9eeaf827f192531177c0549efe67406220fd9fed
Paul Gerhartz <15791412+gerhartz@users.noreply.github.com>
2023-06-28 17:04:13 -0600

    Reproduce Issue
    Created a blank Maui app and added code to MainPage.xaml.cs

---
+++ AndroidCrash/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AndroidCrash;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AndroidCrash/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace AndroidCrash;
+
+public partial class MainPage : ContentPage
+{
+    private readonly View Control = new Label() {
+        Text = "Test"
+    };
+
+    private readonly Button Trigger = new Button() {
+        Text = "Click Me"
+    };
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        Trigger.Clicked += (sender, args) =>
+        {
+            Add();
+        };
+
+        Content = new VerticalStackLayout()
+        {
+            Children =
+            {
+                Trigger,
+                Control
+            }
+        };
+    }
+
+    private void Add()
+    {
+        Content = new VerticalStackLayout()
+        {
+            Control
+        };
+    }
+}
